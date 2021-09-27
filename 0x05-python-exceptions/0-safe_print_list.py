ca66d4a7f3d6440c36832a387c0eb7883e87ffe3
root <knashipa@gmail.com>
2021-09-27 21:35:02 +0000

    Print elements of list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-            count += 1
+            print("{}".format(my_list[i]), end="")
+            total += 1
         except IndexError:
             break
     print()
